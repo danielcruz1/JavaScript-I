55d9408c16923a0ade78e3f99528583ddefc90ad
staccats <47672588+staccats@users.noreply.github.com>
2019-08-09 19:06:02 -0600

    moved console.log(BMWAndAudi) outside of function per TL suggestion

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -124,9 +124,9 @@ let BMWAndAudi = [];
  for (let i = 0; i < inventory.length; i++){
       if (inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi'){
         BMWAndAudi.push(inventory[i]);
-        console.log(BMWAndAudi)
       }
   }
+  console.log(BMWAndAudi)
 
 
 
